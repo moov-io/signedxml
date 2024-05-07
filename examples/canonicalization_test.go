fcb25cb500bd11c704b214c41d8c729fd5694ca6
Adam Shannon <adamkshannon@gmail.com>
2024-05-07 11:25:01 -0500

    examples: run 3.6 in tests

--- examples/canonicalization_test.go
+++ examples/canonicalization_test.go
@@ -191,8 +191,8 @@ func TestCanonicalizationExamples(t *testing.T) {
 			// "(Example 3.5)": {input: example35Input, output: example35Output},
 			// 3.6 will work, but requires a change to the etree package first:
 			// http://stackoverflow.com/questions/6002619/unmarshal-an-iso-8859-1-xml-input-in-go
-			// "(Example 3.6)": {input: example36Input, output: example36Output},
-			// "(Example 3.7)": {input: example37Input, output: example37Output, expression: example37SubsetExpression},
+			"(Example 3.6)": {input: example36Input, output: example36Output},
+			// "(Example 3.7)":                   {input: example37Input, output: example37Output, expression: example37SubsetExpression},
 			"(Example from GitHub Issue #50)": {input: exampleGHIssue50Input, output: exampleGHIssue50Output},
 		}
 		for description, test := range cases {
@@ -214,6 +214,5 @@ func TestTODO(t *testing.T) {
 	// The XML specifications cover the following examples, but our library does not successfully transform.
 	t.Logf("Input:\n%s\nOutput:\n%s\n", example33Input, example33Output) // Example 3.3
 	t.Logf("Input:\n%s\nOutput:\n%s\n", example35Input, example35Output) // Example 3.5
-	t.Logf("Input:\n%s\nOutput:\n%s\n", example36Input, example36Output) // Example 3.6
 	t.Logf("Input:\n%s\nOutput:\n%s\n", example37Input, example37Output) // Example 3.7
 }
